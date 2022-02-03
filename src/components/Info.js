621be24a58a65b0bf89e2e3c894ecd13a484fa00
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-03 21:06:02 +0200

    Added Info and Socials components and styles

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function Info() {
   return (
-    <h1>Info</h1>
+    <div className="info-container">
+      <section className="about-section">
+        <h3>About</h3>
+        <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+      </section>
+      <section className="interest-section">
+        <h3>Interests</h3>
+        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+      </section>
+    </div>
   );
 };
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,16 @@
 import React from "react";
+import twitterIcon from "../images/twitter.svg";
+import facebookIcon from "../images/facebook.svg";
+import instagramIcon from "../images/instagram.svg";
+import githubIcon from "../images/github.svg";
 
 export default function Socials() {
   return (
-    <h1>Socials</h1>
+    <div className="socials-container">
+      <a href="https://twitter.com/"><img src={twitterIcon} /></a>
+      <a href="https://www.facebook.com/"><img src={facebookIcon} /></a>
+      <a href="https://www.instagram.com/"><img src={instagramIcon} /></a>
+      <a href="https://github.com/"><img src={githubIcon} /></a>
+    </div>
   );
 };
